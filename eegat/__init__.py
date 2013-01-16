ff3dea32d9ce1c7010d9e57018b2f195f03e5648
Massimo Santini <santini@dsi.unimi.it>
2013-01-16 11:40:51 +0100

    Now answers to any port

--- eegat/__init__.py
+++ eegat/__init__.py
@@ -97,4 +97,4 @@ def handle():
 			return '# An unexpected error occurred!"\n', 500, { 'Content-Type': 'text/plain' }
 
 if __name__ == '__main__':
-	app.run( port = 8000, debug = True )
+	app.run( host= '0.0.0.0', port = 8000, debug = False )
