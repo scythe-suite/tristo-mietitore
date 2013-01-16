ce50423da17fb1d2af31b17e67619b636c7452bd
Massimo Santini <santini@di.unimi.it>
2013-01-16 02:55:16 +0100

    Added checks to avoid double signatures; added try/catch to make the app more opaque in production (but added logging)

--- eegat/__init__.py
+++ eegat/__init__.py
@@ -1,7 +1,9 @@
 from base64 import encodestring, decodestring
+from errno import EEXIST
 from hmac import new as mac
 from hashlib import sha256
-from os import makedirs
+from logging import StreamHandler, Formatter, INFO
+from os import makedirs, open as os_open, close, write, O_EXCL, O_CREAT, O_WRONLY
 from os.path import join, isdir
 from tarfile import TarFile
 from time import time
@@ -10,9 +12,33 @@ from flask import Flask, render_template, request
 
 app = Flask( __name__ )
 app.config.from_envvar( 'EEGNG_SETTINGS' )
+if not app.debug:
+	sh = StreamHandler()
+	f = Formatter( '%(asctime)s [%(process)s] [%(levelname)s] Flask: %(name)s [in %(pathname)s:%(lineno)d] %(message)s','%Y-%m-%d %H:%M:%S' )
+	sh.setFormatter( f )
+	app.logger.addHandler( sh )
+	app.logger.setLevel( INFO )
+
+def _sign( uid ):
+	return '{0}:{1}'.format( uid, mac( app.config[ 'SECRET_KEY' ], uid, sha256 ).hexdigest() )
 
 def sign( uid ):
-	return '{0}:{1}'.format( uid, mac( app.config[ 'SECRET_KEY' ], uid, sha256 ).hexdigest() )
+	try:
+		dest_dir = join( app.config[ 'UPLOAD_DIR' ], uid )
+		try:
+			makedirs( dest_dir )
+		except OSError as e:
+			if e.errno == EEXIST and isdir( dest_dir ): pass
+			else: raise RuntimeError( '{0} exists and is not a directory'.format( dest_dir ) )
+		fd = os_open( join( dest_dir, 'IP.txt' ), O_CREAT | O_EXCL | O_WRONLY, 0600 )
+	except OSError as e: # already signed
+		if e.errno == EEXIST: signature = None
+		else: raise
+	else:
+		signature = _sign( uid )
+		write( fd, request.remote_addr + '\n' )
+		close( fd )
+	return signature
 
 def check( signature ):
 	try:
@@ -20,40 +46,54 @@ def check( signature ):
 	except ValueError:
 		return False
 	else:
-		return sign( uid ) == signature
+		return _sign( uid ) == signature
 
 @app.route( '/<uid>' )
 def bootstrap( uid ):
 	try:
-		data = app.config[ 'REGISTERED_UIDS' ][ uid ]
-	except KeyError:
-		client = ''
-	else:
-		client = encodestring( render_template( 'client.py', data = data, signature = sign( uid ) ) )
-	return render_template( 'bootstrap.py', client = client, uid = uid ), 200, { 'Content-Type': 'text/plain' }
+		try:
+			data = app.config[ 'REGISTERED_UIDS' ][ uid ]
+		except KeyError:
+			data = None # not registered
+			client = None
+		else:
+			signature = sign( uid )
+			client = encodestring( render_template( 'client.py', data = data, signature = signature ) ) if signature else None
+		return render_template( 'bootstrap.py', client = client, data = data ), 200, { 'Content-Type': 'text/plain' }
+	except:
+		if app.debug:
+			raise
+		else:
+			app.logger.exception( '' )
+			return 'print \'echo "An unexpected error occurred!"\'\n', 200, { 'Content-Type': 'text/plain' }
 
 @app.route( '/', methods = [ 'POST' ] )
 def handle():
 	try:
-		signature = request.form[ 'signature' ]
-	except KeyError:
-		allowed = False
-	else:
-		allowed = check( signature )
-		uid = signature.split( ':' )[ 0 ]
-	if not allowed: return 'print "Invalid or absent signature"', 401, { 'Content-Type': 'text/plain' }
-	if 'tar' in request.form:
-		data = decodestring( request.form[ 'tar' ] )
-		dest_dir = join( app.config[ 'UPLOAD_DIR' ], uid )
-		if not isdir( dest_dir ): makedirs( dest_dir )
-		dest = join( dest_dir, str( int( time() * 1000 ) ) + '.tar' )
-		with open( dest, 'w' ) as f: f.write( data )
-		tf = TarFile.open( dest, mode = 'r' )
-		names = tf.getnames()
-		tf.close()
-		return '\n'.join( names ), 200, { 'Content-Type': 'text/plain' }
-	else:
-		return app.config[ 'DOWNLOAD_BUNDLE' ], 200, { 'Content-Type': 'text/plain' }
+		try:
+			signature = request.form[ 'signature' ]
+		except KeyError:
+			allowed = False
+		else:
+			allowed = check( signature )
+			uid = signature.split( ':' )[ 0 ]
+		if not allowed: return '# Invalid or absent signature!\n', 401, { 'Content-Type': 'text/plain' }
+		if 'tar' in request.form: # this is an upload
+			data = decodestring( request.form[ 'tar' ] )
+			dest = join( app.config[ 'UPLOAD_DIR' ], uid, str( int( time() * 1000 ) ) + '.tar' )
+			with open( dest, 'w' ) as f: f.write( data )
+			tf = TarFile.open( dest, mode = 'r' )
+			names = tf.getnames()
+			tf.close()
+			return '\n'.join( names ), 200, { 'Content-Type': 'text/plain' }
+		else: # this is a download
+			return app.config[ 'DOWNLOAD_BUNDLE' ], 200, { 'Content-Type': 'text/plain' }
+	except:
+		if app.debug:
+			raise
+		else:
+			app.logger.exception( '' )
+			return '# An unexpected error occurred!"\n', 500, { 'Content-Type': 'text/plain' }
 
 if __name__ == '__main__':
 	app.run( port = 8000, debug = True )
