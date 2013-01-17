871a892ae8ec706484a681c8f0ec7c19edf16356
Massimo Santini <santini@di.unimi.it>
2013-01-17 14:52:00 +0100

    Added the possibility to specify verbosity and a regexp to filter tar data

--- tm/mkconf.py
+++ tm/mkconf.py
@@ -11,23 +11,26 @@ tar = loc[ 'tar' ]
 def main():
 
 	parser = argparse.ArgumentParser()
-	parser.add_argument( 'output_conf_file', help = 'The resulting configuration file' )
-	parser.add_argument( 'base_conf_file', help = 'The configuration file to start with' )
+	parser.add_argument( 'output_conf', help = 'The resulting configuration file' )
+	parser.add_argument( 'base_conf', help = 'The configuration file to start with' )
 	parser.add_argument( 'tar_dir', help = 'The directory to include as TAR_DATA' )
-	parser.add_argument( 'registerd_uids_file', help = 'A tab separated file of (uid, data) pairs to include as REGISTERED_UIDS', nargs = '?' )
+	parser.add_argument( 'registerd_uids', help = 'A tab separated file of (uid, data) pairs to include as REGISTERED_UIDS', nargs = '?' )
+	parser.add_argument( '--filter', '-f', default = '.*', help = 'A regular expression files to be included in TAR_DATA must match' )
+	parser.add_argument( '--verbose', '-v', default = False, action='store_true', help = 'Whether to show the files added to TAR_DATA' )
+
 	args = parser.parse_args()
 
-	with open( args.base_conf_file, 'r' ) as f: base_conf = f.read()
+	with open( args.base_conf, 'r' ) as f: base_conf = f.read()
 
-	tar_data = tar( args.tar_dir, verbose = False )
+	tar_data = tar( args.tar_dir, args.filter, args.verbose )
 
-	if args.registerd_uids_file:
-		with open( args.registerd_uids_file, 'r' ) as f:
+	if args.registerd_uids:
+		with open( args.registerd_uids, 'r' ) as f:
 			registered_uids = dict( ( line.strip().split( '\t' ) for line in f if line != '\n' and not line.startswith( '#' ) ) )
 	else:
 		registered_uids = None
 
-	with open( args.output_conf_file, 'w' ) as f:
+	with open( args.output_conf, 'w' ) as f:
 		f.write( base_conf + '\n' )
 		f.write( 'TAR_DATA = """\n' + tar_data + '"""\n' )
 		if registered_uids: f.write( '\nREGISTERED_UIDS = ' + repr( registered_uids ) + '\n' )
