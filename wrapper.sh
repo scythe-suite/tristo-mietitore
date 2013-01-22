8a9e9b72bd8d0795264f0fae7dfd8cfaa1cd1b75
Massimo Santini <santini@di.unimi.it>
2013-01-22 10:55:48 +0100

    Fixing logging

--- wrapper.sh
+++ wrapper.sh
@@ -4,7 +4,7 @@
 trap 'echo wrapper killing $PID; kill -QUIT $PID' TERM
 
 # program to run
-gunicorn tm.web:app --logger-class nofussbm.logger.Logger --access-logfile=/dev/null --error-logfile=- -w 3 -b 0.0.0.0:$PORT &
+gunicorn tm.web:app --error-logfile=- -w 3 -b 0.0.0.0:$PORT &
 
 # capture PID and wait
 PID=$!
