53f56863c77bd7b3de273fa7603b92e06ddb0dad
Massimo Santini <santini@di.unimi.it>
2013-01-28 16:12:50 +0100

    Better __main__ based on import_module (to avoid surious web initialization)
    --HG--
    extra : rebase_source : cf7a069f239ed503722496bd4ab1b301f87a6c6c

--- src/__main__.py
+++ src/__main__.py
@@ -1,15 +1,10 @@
+from importlib import import_module
 from sys import argv
 
-from tm import mkconf, web
-
 if __name__ == '__main__':
-	dispatch = {
-		'web': web.main,
-		'mkconf': mkconf.main
-	}
 	try:
-		dispatch[ argv.pop( 1 ) ]()
-	except ( IndexError, KeyError ):
+		import_module( 'tm.{0}'.format( argv.pop( 1 ) ) ).main()
+	except ( IndexError, ImportError ):
 		print 'usage: python {0} {{web,mkconf}} ...'.format( argv[ 0 ] )
 	except:
 		raise
