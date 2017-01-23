c5e5e13246d4d0927a1072d1f10c6e9bc9d85420
Massimo Santini <massimo.santini@gmail.com>
2017-01-24 00:18:04 +0100

    Better handling of subommands

--- src/__main__.py
+++ src/__main__.py
@@ -1,11 +1,25 @@
 from importlib import import_module
-from sys import argv
+from sys import argv, stderr, exit
 from os.path import dirname
 
+from tm import VERSION
+
+COMMANDS = 'web', 'mkconf', 'untarconf', 'hashconf', 'mkresults'
+
 if __name__ == '__main__':
-	try:
-		import_module( 'tm.{0}'.format( argv.pop( 1 ) ) ).main()
-	except ( IndexError, ImportError ):
-		print 'usage: tm {web,mkconf,untarconf,hashconf,mkresults} ...'
-	except:
-		raise
+    try:
+        subcommand = argv.pop( 1 )
+    except IndexError:
+        stderr.write( 'tm: subcommsands: {}\n'.format( ', '.join( COMMANDS ) ) )
+        exit( 1 )
+    if subcommand == 'version':
+        stderr.write( 'tm: version: {}\n'.format( VERSION ) )
+        exit( 0 )
+    if subcommand not in COMMANDS:
+        stderr.write( 'tm: unknown subcommand {}; available subcommsands: {}\n'.format( subcommand, ', '.join( COMMANDS ) ) )
+        exit(1)
+    try:
+        import_module( 'tm.{0}'.format( subcommand ) ).main()
+    except KeyboardInterrupt:
+        stderr.write( 'tm: premature exit!\n' )
+        exit( 1 )
